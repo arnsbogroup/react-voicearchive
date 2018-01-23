36f5bc93a50c01579eeed48e49d2ce61422187c2
Mads Lundholm <mads@madslundholm.dk>
2018-01-23 15:16:03 +0100

    Implements a simple Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  background-color: rgb(250, 250, 250);
+  border-bottom: 1px solid rgb(235, 235, 235);
+  overflow: hidden;
+  padding: 0 15px;
+`
