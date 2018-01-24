6b6fec59518f2047135af9a79107afc178b06419
Mads Lundholm <mads@madslundholm.dk>
2018-01-24 14:12:00 +0100

    Remove the usage of React since we use styled-components

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-/* eslint-enable no-unused-vars */
 import styled from 'styled-components';
 
 const baseUrl = 'https://branding.arnsbomedia.com/voicearchive/logo/';
